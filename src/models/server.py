f83e2a7da0b1ea8455753d40578139de01d842f4
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2024-05-22 23:49:13 -0500

    Funcional

--- src/models/server.py
+++ src/models/server.py
@@ -19,11 +19,10 @@ class Server1:
             patient.end_time = patient.start_time + service_time
             self.free_time = patient.end_time
             queue_2.put((patient.end_time, patient.name))
-            print(f"->{patient.name} llega a la cola 1 a las {patient.arrival_time:.3f}, es atendido: {patient.start_time:.3f} y termina a las {patient.end_time:.3f}, tiempo servicio {service_time:.3f}, tiempo espera {patient.waiting_time:.3f}")
+            print(f"->{patient.name} llega a la cola 1 a las {patient.arrival_time:.3f}, es atendido: {patient.start_time:.3f} y termina a las {patient.end_time:.3f}, tiempo servicio {service_time:.3f}, tiempo espera {patient.waiting_time:.3f}\n")
             self.log.append((patient.name, patient.arrival_time, patient.start_time, service_time, patient.end_time, patient.waiting_time))
             self.global_clock[0] = max(self.global_clock[0], patient.end_time)
             time.sleep(service_time * 10)
-    
 
 class Server2:
     def __init__(self, global_clock, server_id, rate):
@@ -41,10 +40,10 @@ class Server2:
             end_time_2 = start_time_2 + service_time
             self.free_time = end_time_2
             waiting_time_2 = start_time_2 - end_time_1
-            print(f"-->{patient_name} llega a la cola 2 a las {end_time_1:.3f}, es atendido por el servidor {self.server_id} a las {start_time_2:.3f} y termina a las {end_time_2:.3f}, tiempo servicio {service_time:.3f}")
+            print(f"-->{patient_name} llega a la cola 2 a las {end_time_1:.3f}, es atendido por el servidor {self.server_id} a las {start_time_2:.3f} y termina a las {end_time_2:.3f}, tiempo servicio {service_time:.3f}\n")
             if listRi.get_next_ri() < 0.25:
                 queue_3.put((end_time_2, patient_name))
-                print(f"#{patient_name} del servidor {self.server_id} necesita medicamentos y se dirige a la cola 3 a las {end_time_2:.3f}")
+                print(f"#{patient_name} del servidor {self.server_id} necesita medicamentos y se dirige a la cola 3 a las {end_time_2:.3f}\n")
             self.log.append((patient_name, end_time_1, start_time_2, service_time, end_time_2, waiting_time_2))
             self.global_clock[0] = max(self.global_clock[0], end_time_2)
             time.sleep(service_time * 10)
@@ -64,7 +63,7 @@ class Server4:
             end_time_3 = start_time_3 + service_time
             self.free_time = end_time_3
             waiting_time_3 = start_time_3 - end_time_2
-            #print(f"--->{patient_name} llega a la cola 3 a las {end_time_2:.3f}, es atendido por el servidor 4 a las {start_time_3:.3f} y termina a las {end_time_3:.3f}, tiempo servicio {service_time:.3f}")
+            print(f"--->{patient_name} llega a la cola 3 a las {end_time_2:.3f}, es atendido por el servidor 4 a las {start_time_3:.3f} y termina a las {end_time_3:.3f}, tiempo servicio {service_time:.3f}\n")
             self.log.append((patient_name, end_time_2, start_time_3, service_time, end_time_3, waiting_time_3))
             self.global_clock[0] = max(self.global_clock[0], end_time_3)
             time.sleep(service_time * 10)
