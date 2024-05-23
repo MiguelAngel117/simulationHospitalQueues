cff2d65686c97dc85f8452c1c09018d873047a9f
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2024-05-23 13:22:47 -0500

    m

--- src/helpers/generateNames.py
+++ src/helpers/generateNames.py
@@ -3,7 +3,7 @@ def generateNames():
     male_first_names = ['Juan', 'Pedro', 'Luis', 'Carlos', 'Alex', 'Andres', 'Miguel', 'Benito', 'José', 'Diego', 'Alejandro', 'Manuel', 'Fabian', 'Daniel']
     female_first_names = ['María', 'Ana', 'Sofía', 'Laura', 'Andrea', 'Luisa', 'Rogelia', 'Josefina', 'Danna', 'Magdalena', 'Lupe', 'Julia', 'Victoria' ]
     
-    male_second_names = ['Ivan', 'Antonio', 'Angel', 'Javier', 'Diego', 'Jorge', 'Ricardo', 'Carlos' , 'Andres', 'Felipe']
+    male_second_names = ['Ivan', 'Antonio', 'Angel', 'Javier', 'Diego', 'Jorge', 'Ricardo', 'Carlos' , 'Andres', 'Felipe' 'Antonio', 'Dario']
     female_second_names = ['Isabel', 'Gabriela', 'Valentina', 'Camila', 'Daniela', 'Lucía', 'Karina', 'Angela', 'Dora', 'Elena', 'Milena', 'Mariela']
 
     if random.choice(['male', 'female']) == 'male':
