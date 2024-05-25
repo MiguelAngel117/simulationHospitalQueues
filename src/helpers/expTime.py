ba25456474f6496efd7cadd32dfdcdb1f997ef7c
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2024-05-25 09:56:38 -0500

    exp time

--- src/helpers/expTime.py
+++ src/helpers/expTime.py
@@ -1,4 +1,14 @@
 import numpy as np
 
 def exp_time(rate, ri):
+    """
+    Calcula el tiempo de un evento según una distribución exponencial.
+
+    Parámetros:
+    - rate (float): Tasa de llegada de eventos (parámetro lambda o miu).
+    - ri (float): Número aleatorio generado entre 0 y 1.
+
+    Retorna:
+    - float: Tiempo del próximo evento según la distribución exponencial.
+    """
     return -(1 / rate) * np.log(1 - ri)
